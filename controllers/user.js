04c12ef6fa09ded2ba060830a1c3cac74f6c357a
MaximeLorin <maxime.lorin56@gmail.com>
2021-06-09 15:29:37 +0200

    add gitignore

--- controllers/user.js
+++ controllers/user.js
@@ -35,11 +35,6 @@ exports.login = (req, res, next) => {
           if (!valid) {
             return res.status(401).json({ error: "Mot de passe non valide !" });
           }
-          console.log({
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-          });
           res.status(200).json({
             userId: user._id,
             token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
